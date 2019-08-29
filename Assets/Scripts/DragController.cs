ae613825de17c23f80ca1ef500d2a98e2434b465
JEunLe <hemslje@naver.com>
2019-08-30 08:38:41 +0900

    CardFlipAnimation

--- Assets/Scripts/DragController.cs
+++ Assets/Scripts/DragController.cs
@@ -9,27 +9,37 @@ public class DragController : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
     Vector3 startPosition;
     Transform startParent;
 
+    
+    // OnBeginDrag - 드래그 할때 처음 한번 호출
     public void OnBeginDrag(PointerEventData eventData)
     {
         itemBeginDragged = gameObject;
         startPosition = transform.position;
-        startParent = transform.parent;
+        startParent = transform.parent; //slot name will be returned
+        /* 
+         * Raycast - 공간의 특정 지점에서 특정 방향 거리 안에 게임 오브젝트가 존재하는지 판별
+         * blockRaycast - Canvas가 Raycast와 충돌이 가능하게함 
+         */
+         // GetComponent<CanvasGroup>().blocksRaycasts = true로 하면 카드 드래그해서 슬롯에 둘 수 없음
         GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
 
+    // OnDrag - 드래그 동안 마우스 커서가 움직일때마다 호출
     public void OnDrag(PointerEventData eventData)
     {
         transform.position = Input.mousePosition;
     }
 
+    // OnEndDrag - 드래그 종료시 한번 호출
     public void OnEndDrag(PointerEventData eventData)
     {
         itemBeginDragged = null;
         GetComponent<CanvasGroup>().blocksRaycasts = true;
-        if(transform.parent == startParent)
+        if (transform.parent == startParent)
         {
             transform.position = startPosition;
         }
-        
+
     }
+
 }
---
+++ Assets/Scripts/Flip.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : MonoBehaviour
+{
+    public int fps = 60;
+    public float rotateDegreePerSecone = 180f;
+    public bool isFaceUp = false;
+
+    const float FLIP_LIMIT_DEGREE = 180f;
+
+    float waitTime;
+    bool isAnimationProcessing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitTime = 1.0f / fps;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (isAnimationProcessing)
+        {
+            return;
+        }
+        StartCoroutine(flip());
+    }
+
+    IEnumerator flip()
+    {
+        isAnimationProcessing = true;
+        bool done = false;
+        while (!done)
+        {
+            float degree = rotateDegreePerSecone * Time.deltaTime;
+            if (isFaceUp)
+            {
+                degree = -degree;
+            }
+            transform.Rotate(new Vector3(0, degree, 0));
+
+            if (FLIP_LIMIT_DEGREE < transform.eulerAngles.y)
+            {
+                transform.Rotate(new Vector3(0, -degree, 0));
+                done = true;
+            }
+            yield return new WaitForSeconds(0.01f);
+        }
+        isFaceUp = !isFaceUp;
+        isAnimationProcessing = false;
+    }
+}
